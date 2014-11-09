be8ace0d7d5136de5864d20148aeff4b1254b10d
Christoph AuerWelsbach <awchristoph@gmail.com>
2014-11-09 22:15:26 +0000

    installed devise in application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    # Default URL to be set
+  config.action_mailer.default_url_options = { host: bitter-awchristoph.herokuapp.com }
 end
